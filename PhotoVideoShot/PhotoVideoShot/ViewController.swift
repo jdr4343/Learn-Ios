ed5b4209e1f1dd3118b2cebb6a2fdf2292510f5d
jdr4343 <jdr4343@naver.com>
2021-08-10 16:00:07 +0900

    pod init

--- PhotoVideoShot/PhotoVideoShot/ViewController.swift
+++ PhotoVideoShot/PhotoVideoShot/ViewController.swift
@@ -9,8 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    
+    @IBOutlet weak var profileChange: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.profileImage.layer.cornerRadius = profileImage.frame.height/2
+        self.profileChange.layer.cornerRadius = 10
+        
+        //버튼을 액션으로 끌어오는 방법도 있지만 많이 경험해 보았으므로 코드로 연결해보겠습니다.
+        self.profileChange.addTarget(self, action: #selector(ProfileChangeTapped), for: .touchUpInside)
+        
+    }
+        @objc private func ProfileChangeTapped() {po
+            print("Tapped ProfileChangeTapped")
+        
+        
         // Do any additional setup after loading the view.
     }
 
---
+++ PhotoVideoShot/Pods/Target Support Files/Pods-PhotoVideoShot/Pods-PhotoVideoShot-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhotoVideoShot : NSObject
+@end
+@implementation PodsDummy_Pods_PhotoVideoShot
+@end
