5ed02c5d0e059bcba4b7402505d1e4379add71c7
jdr4343 <jdr4343@naver.com>
2021-08-10 16:54:53 +0900

    finished Photo

--- PhotoVideoShot/PhotoVideoShot/ViewController.swift
+++ PhotoVideoShot/PhotoVideoShot/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import YPImagePicker
 
 class ViewController: UIViewController {
 
@@ -26,11 +27,35 @@ class ViewController: UIViewController {
         self.profileChange.addTarget(self, action: #selector(ProfileChangeTapped), for: .touchUpInside)
         
     }
-        @objc private func ProfileChangeTapped() {po
+        @objc private func ProfileChangeTapped() {
             print("Tapped ProfileChangeTapped")
+            //카메라 라이브러리 설정
+            var config = YPImagePickerConfiguration()
+            config.screens = [.library, .video, .photo]
+           
+
+
+             //사진이 선택됬을때
+            let picker = YPImagePicker(configuration: config)
+            
+            picker.didFinishPicking { [unowned picker] items, _ in
+                if let photo = items.singlePhoto {
+                    print(photo.fromCamera) // Image source (camera or library)
+                    print(photo.image) // Final image selected by the user
+                    print(photo.originalImage) // original image selected by the user, unfiltered
+                    print(photo.modifiedImage) // Transformed image, can be nil
+                    print(photo.exifMeta) // Print exif meta data of original image.
+                    //프사 이미지 변경
+                    self.profileImage.image = photo.image
+                }
+                //사진 선택창 닫기
+                picker.dismiss(animated: true, completion: nil)
+            }
+            //사진 선택창 보여주기
+            present(picker, animated: true, completion: nil)
+
         
         
-        // Do any additional setup after loading the view.
     }
 
 
