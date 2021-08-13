e9c4095c8c519c1f857bc5c1d00c6fe77344bfdb
jdr4343 <jdr4343@naver.com>
2021-08-14 08:18:51 +0900

    finish

--- DateFormatter/DateFormatter/ViewController.swift
+++ DateFormatter/DateFormatter/ViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
+    let formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        //스타일 변경
+        
+        //장치의 시간을 현재 시간대로 지정
+        formatter.timeZone = .current
+        formatter.locale = .current
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //현재 날짜를 문자열로 선언
+        let date = Date()
+        
+        
+        label.text = formatter.string(from: date)
     }
 
 
