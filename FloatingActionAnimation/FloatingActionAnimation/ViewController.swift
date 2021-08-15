a7dcf43a5127b3fd4144563dbad027a7a8024387
jdr4343 <jdr4343@naver.com>
2021-08-16 03:18:52 +0900

    Umm..

--- FloatingActionAnimation/FloatingActionAnimation/ViewController.swift
+++ FloatingActionAnimation/FloatingActionAnimation/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import Floaty
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,FloatyDelegate{
+
+    let floaty = Floaty()
+    let item = FloatyItem()
+    
+     
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.addSubview(floaty)
+        addFloaty()
+        
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        floaty.frame = CGRect(x: view.frame.size.width - 60 - 20,
+                              y: view.frame.size.height - 120,
+                              width: 70, height: 70)
+        
+    }
+    
+    fileprivate func addFloaty() {
+        floaty.buttonColor = UIColor.systemIndigo
+        floaty.addItem("만들었습니다.", icon: UIImage(systemName: "pencil"), handler: { [self] item in
+            if let uvc = self.storyboard?.instantiateViewController(identifier: "SempleViewVC") {
+                uvc.modalTransitionStyle = UIModalTransitionStyle.coverVertical
+                        self.present(uvc, animated: true)
+            }
+        })
+        floaty.addItem("플로팅 버튼을", icon: UIImage(systemName: "person"), handler: { item in
+            
+        })
+        floaty.addItem("오늘은", icon: UIImage(named: "icon"))
+        floaty.addItem("신지훈입니다", icon: UIImage(named: "icon"))
+    }
+    
+    
+    
 }
-
--- FloatingActionAnimation/Pods/Target Support Files/Pods-FloatingActionAnimation/Pods-FloatingActionAnimation-frameworks.sh
+++ FloatingActionAnimation/Pods/Target Support Files/Pods-FloatingActionAnimation/Pods-FloatingActionAnimation-frameworks.sh
@@ -176,13 +176,9 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Floaty/Floaty.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/VBFPopFlatButton/VBFPopFlatButton.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/pop/pop.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Floaty/Floaty.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/VBFPopFlatButton/VBFPopFlatButton.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/pop/pop.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
